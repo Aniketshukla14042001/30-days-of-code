b10d31513f84380c1244df04879d76734395b1f6
Aniketshukla14042001 <73159080+Aniketshukla14042001@users.noreply.github.com>
2021-01-23 12:14:46 +0530

    Add files via upload

---
+++ Day1.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+	
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        Scanner scan = new Scanner(System.in);
+        /* Declare second integer, double, and String variables. */
+int k=scan.nextInt();
+double l=scan.nextDouble();
+String stp=scan.nextLine();
+String str=scan.nextLine();
+System.out.println(i+k);
+System.out.println(d+l);
+System.out.println(s+str);
+        /* Print the sum of both integer variables on a new line. */
+
+        /* Print the sum of the double variables on a new line. */
+		
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+
+        scan.close();
+    }
+}
